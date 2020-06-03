2e2983b1fdcd2635bdbfd3de04cedfc18c8d0ab7
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-06-03 16:26:38 +0100

    Check if file has been downloaded already

--- era_downloader.py
+++ era_downloader.py
@@ -55,55 +55,61 @@ def grab_era5(month, year, output_folder, region, mylat, mylon):
     mylon : float
         Longitude in decimal degrees.
     """
-    #'80/-50/-25/0', # North, West, South, East.
-    area = f"{int(mylat[0]):d}/{int(mylon[0]):d}/" + \
-           f"{int(mylat[1]):d}/{int(mylon[1]):d}"
-    #[60, -10, 50, 2], # North, West, South, East
-    c = cdsapi.Client()                
-    c.retrieve(
-        'reanalysis-era5-land',
-        {
-            'variable':[
-                'surface_solar_radiation_downwards',
-                'total_precipitation',
-                '10m_u_component_of_wind','10m_v_component_of_wind',
-                '2m_dewpoint_temperature',   
-                '2m_temperature', 'evaporation_from_the_top_of_canopy',
-                'evaporation_from_vegetation_transpiration',
-                'evapotranspiration', 'potential_evaporation',
-                'volumetric_soil_water_layer_1'                         
-            ],
-            'product_type':'reanalysis',
-            'year':f"{year:d}",
-            'month':f"{month:02d}",
-            'day':[
-                '01','02','03',
-                '04','05','06',
-                '07','08','09',
-                '10','11','12',
-                '13','14','15',
-                '16','17','18',
-                '19','20','21',
-                '22','23','24',
-                '25','26','27',
-                '28','29','30',
-                '31'
-            ],
-            'time':[
-                '00:00','01:00','02:00',
-                '03:00','04:00','05:00',
-                '06:00','07:00','08:00',
-                '09:00','10:00','11:00',
-                '12:00','13:00','14:00',
-                '15:00','16:00','17:00',
-                '18:00','19:00','20:00',
-                '21:00','22:00','23:00'
-            ],
-            #'area' : "%d/%d/%d/%d"%
-            'area' : area,
-            'format':'netcdf'
-        },
-        (Path(output_folder)/f"{region:s}.{year:d}_{month:02d}.nc").as_posix())
+    output_nc_file = (Path(output_folder)/f"{region:s}.{year:d}_{month:02d}.nc")
+    if not output_nc_file.exists():
+
+        #'80/-50/-25/0', # North, West, South, East.
+        area = f"{int(mylat[0]):d}/{int(mylon[0]):d}/" + \
+            f"{int(mylat[1]):d}/{int(mylon[1]):d}"
+        #[60, -10, 50, 2], # North, West, South, East
+        c = cdsapi.Client()                
+        c.retrieve(
+            'reanalysis-era5-land',
+            {
+                'variable':[
+                    'surface_solar_radiation_downwards',
+                    'total_precipitation',
+                    '10m_u_component_of_wind','10m_v_component_of_wind',
+                    '2m_dewpoint_temperature',   
+                    '2m_temperature', 'evaporation_from_the_top_of_canopy',
+                    'evaporation_from_vegetation_transpiration',
+                    'evapotranspiration', 'potential_evaporation',
+                    'volumetric_soil_water_layer_1'                         
+                ],
+                'product_type':'reanalysis',
+                'year':f"{year:d}",
+                'month':f"{month:02d}",
+                'day':[
+                    '01','02','03',
+                    '04','05','06',
+                    '07','08','09',
+                    '10','11','12',
+                    '13','14','15',
+                    '16','17','18',
+                    '19','20','21',
+                    '22','23','24',
+                    '25','26','27',
+                    '28','29','30',
+                    '31'
+                ],
+                'time':[
+                    '00:00','01:00','02:00',
+                    '03:00','04:00','05:00',
+                    '06:00','07:00','08:00',
+                    '09:00','10:00','11:00',
+                    '12:00','13:00','14:00',
+                    '15:00','16:00','17:00',
+                    '18:00','19:00','20:00',
+                    '21:00','22:00','23:00'
+                ],
+                #'area' : "%d/%d/%d/%d"%
+                'area' : area,
+                'format':'netcdf'
+            },
+            output_nc_file.as_posix())
+            return output_nc_file.as_posix()
+        else
+            return None
 
         
 if __name__ == "__main__":
