3c66136a3ae208a08f2af8740a9eec45303b11a4
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-06-30 15:37:36 +0100

    bug fixes

--- era_downloader.py
+++ era_downloader.py
@@ -63,7 +63,7 @@ def grab_era5(month, year, output_folder, region, mylat, mylon):
     mylon : float
         West and East Longitudes in decimal degrees.
     """
-    output_nc_file = (Path(output_folder)/f"{region:s}.{year:d}_{month:02d}.nc")
+    output_nc_file = (Path(output_folder)/f"ERA5_{region:s}.{year:d}_{month:02d}.nc")
     if not output_nc_file.exists():
 
         #'80/-50/-25/0', # North, West, South, East.
@@ -73,47 +73,48 @@ def grab_era5(month, year, output_folder, region, mylat, mylon):
         c = cdsapi.Client()                
         c.retrieve('reanalysis-era5-single-levels',
         {
-        'format': 'netcdf',
-        'variable': [
-            '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
-            '2m_temperature', 'evaporation', 'potential_evaporation',
-            'surface_solar_radiation_downwards', 'total_precipitation', 'volumetric_soil_water_layer_1',
-            'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3', 'volumetric_soil_water_layer_4',
-        ],
-                'product_type':'reanalysis',
-                'year':f"{year:d}",
-                'month':f"{month:02d}",
-                'day':[
-                    '01','02','03',
-                    '04','05','06',
-                    '07','08','09',
-                    '10','11','12',
-                    '13','14','15',
-                    '16','17','18',
-                    '19','20','21',
-                    '22','23','24',
-                    '25','26','27',
-                    '28','29','30',
-                    '31'
-                ],
-                'time':[
-                    '00:00','01:00','02:00',
-                    '03:00','04:00','05:00',
-                    '06:00','07:00','08:00',
-                    '09:00','10:00','11:00',
-                    '12:00','13:00','14:00',
-                    '15:00','16:00','17:00',
-                    '18:00','19:00','20:00',
-                    '21:00','22:00','23:00'
-                ],
-                #'area' : "%d/%d/%d/%d"%
-                'area' : area,
-                'format':'netcdf'
+            'format': 'netcdf',
+            'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', 
+                        '2m_dewpoint_temperature', '2m_temperature', 'evaporation', 
+                        'potential_evaporation', 'surface_solar_radiation_downwards',
+                        'total_precipitation', 'volumetric_soil_water_layer_1',
+                        'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3',
+                        'volumetric_soil_water_layer_4',
+                        ],
+            'product_type':'reanalysis',
+            'year':f"{year:d}",
+            'month':f"{month:02d}",
+            'day':[
+                '01','02','03',
+                '04','05','06',
+                '07','08','09',
+                '10','11','12',
+                '13','14','15',
+                '16','17','18',
+                '19','20','21',
+                '22','23','24',
+                '25','26','27',
+                '28','29','30',
+                '31'
+            ],
+            'time':[
+                '00:00','01:00','02:00',
+                '03:00','04:00','05:00',
+                '06:00','07:00','08:00',
+                '09:00','10:00','11:00',
+                '12:00','13:00','14:00',
+                '15:00','16:00','17:00',
+                '18:00','19:00','20:00',
+                '21:00','22:00','23:00'
+            ],
+            #'area' : "%d/%d/%d/%d"%
+            'area' : area,
+            'format':'netcdf'
             },
-            output_nc_file.as_posix())
-            return output_nc_file.as_posix()
-        else
-            return None
+        output_nc_file.as_posix())
+        return output_nc_file.as_posix()
+    else:
+        return None
 
         
 if __name__ == "__main__":
