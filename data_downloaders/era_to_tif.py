94100fca51297725731d57a27acff1e9f2397084
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-07-21 16:43:06 +0100

    Need to change paths to JASMIN

---
+++ data_downloaders/era_to_tif.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import datetime as dt
+from pathlib import Path 
+
+import gdal
+import osr
+import xarray as xr
+import numpy as np
+
+
+def write_tif(arr, var, year, loc, geoT, srs):
+    n_bands, ny, nx = arr.shape
+    fname_out = (loc/f"{var}_{year}.tif").as_posix()
+    drv = gdal.GetDriverByName("GTiff")
+    ds = drv.Create(fname_out,
+                        nx, ny, n_bands, gdal.GDT_Float32,
+                        options=["COMPRESS=DEFLATE", "TILED=YES", 
+                                 "BIGTIFF=YES", "PREDICTOR=1"])
+    ds.SetGeoTransform(geoT)
+    ds.SetProjection(srs)
+    this_time = dt.date(year, 1, 1)
+    delta = dt.timedelta(days=1)
+    for band in range(n_bands):
+        data_band = ds.GetRasterBand(band+1)
+        data_band.SetMetadata({"DoY":f"{band+1}",
+                               "Date":this_time.strftime("%Y-%m-%d")})
+        data_band.WriteArray(arr[band, :, :])
+        this_time += delta
+    ds = None
+    print(f"{fname_out} saved")
+
+    
+def to_sensible_format():
+
+    p = Path("/data/geospatial_08/ucfajlg/ERA5_meteo/UK/")
+    g = gdal.Open('NETCDF:"/data/geospatial_08/ucfajlg/ERA5_meteo/UK/netcdf/UK.2019_04.nc":ssrd')
+    geoT = g.GetGeoTransform()
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(4326)
+    srs = srs.ExportToWkt()
+    
+    nx, ny = g.RasterXSize, g.RasterYSize
+    for year in range(2000, 2021):
+        fnames = sorted([f for f in (p/"netcdf").glob(f"UK.{year}_??.nc")])   
+        ds=xr.concat([xr.open_dataset(f, chunks={}, mask_and_scale=True ) 
+                    for f in fnames], 
+                    "time") 
+        ssrd = ds.ssrd.resample(time="1D").mean()/1000.
+        write_tif(ssrd.values, "ssrd", year, p, geoT, srs)
+        tp = ds.tp.resample(time="1D").sum()*1000.
+        tp= xr.where(tp >= 0.001, tp, 0)
+        write_tif(tp.values, "precip", year, p, geoT, srs)
+        t2m = ds.t2m.resample(time="1D").mean() - 273.15
+        write_tif(t2m.values, "t2m_mean", year, p, geoT, srs)
+        t2m = ds.t2m.resample(time="1D").min() - 273.15
+        write_tif(t2m.values, "t2m_min", year, p, geoT, srs)
+        t2m = ds.t2m.resample(time="1D").max() - 273.15
+        write_tif(t2m.values, "t2m_max", year, p, geoT, srs)
+        tdew = ds.d2m.resample(time="1D").mean() - 273.15
+        tmp = (17.27 * tdew) / (tdew + 237.3)
+        ea = 0.6108 * np.exp(tmp)
+        write_tif(ea.values, "hum", year, p, geoT, srs)
+        u10 = ds.u10.resample(time="1D").mean()
+        v10 = ds.v10.resample(time="1D").mean()
+        wspd = np.sqrt(u10**2 + v10**2)
+        write_tif(wspd.values, "wspd", year, p, geoT, srs)
+
