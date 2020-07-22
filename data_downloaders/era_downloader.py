122bcfe9a33a29db7abd9276c6fbbc8b491c2b0f
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-07-22 15:56:23 +0100

    Not need to print

--- data_downloaders/era_downloader.py
+++ data_downloaders/era_downloader.py
@@ -138,4 +138,4 @@ if __name__ == "__main__":
         for year in years:
             for month in months:
                 executor.submit(wrapper, month, year)
-    print(executor.result())
+    
