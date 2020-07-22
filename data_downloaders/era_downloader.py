7a834904b8e85adfefef15881e651699f15a1571
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-07-22 15:49:20 +0100

    Sensible download policy
    Only download newish data (120 days, need to re-download files due  to
    ERA5 NRT weirdness, also do not download future stuff that isn't there
    yet!
    
    Changed folder to blahblah/netcdf

--- data_downloaders/era_downloader.py
+++ data_downloaders/era_downloader.py
@@ -3,13 +3,13 @@
 to CABO format to use within WOFOST. So far, using ERA5
 """
 import struct
-import logging 
+import logging
 
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 import datetime as dt
-from itertools import product 
+from itertools import product
 
 from textwrap import dedent
 
@@ -26,18 +26,21 @@ from netCDF4 import Dataset, date2index
 import cdsapi
 
 
-ERAPARAMS = namedtuple("ERAPARAMS", ["ssrd", "mx2t", "mn2t", "tp", 
-                                     "u10", "v10", "d2m"])
+ERAPARAMS = namedtuple(
+    "ERAPARAMS", ["ssrd", "mx2t", "mn2t", "tp", "u10", "v10", "d2m"]
+)
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
 if not LOG.handlers:
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - ' +
-                                  '%(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - " + "%(levelname)s - %(message)s"
+    )
     ch.setFormatter(formatter)
     LOG.addHandler(ch)
 LOG.propagate = False
+
 
 def grab_era5(month, year, output_folder, region, mylat, mylon):
     """A function to download ERA5 Lande data for one month. Assumes
@@ -63,71 +66,73 @@ def grab_era5(month, year, output_folder, region, mylat, mylon):
     mylon : float
         West and East Longitudes in decimal degrees.
     """
-    output_nc_file = (Path(output_folder)/f"ERA5_{region:s}.{year:d}_{month:02d}.nc")
-    if not output_nc_file.exists():
+    output_nc_file = (
+        Path(output_folder) / f"ERA5_{region:s}.{year:d}_{month:02d}.nc"
+    )
+    # This is needed to keep getting the updated ERA5 datasets
+    today = dt.datetime.now()
+    delta_t = today - dt.datetime(year, month, 1)
+    if not output_nc_file.exists() and (
+                    0 <= delta_t.days <= 120 ):
 
+        LOG.info(f"Downloading {year}-{month}")
         #'80/-50/-25/0', # North, West, South, East.
-        area = f"{int(mylat[0]):d}/{int(mylon[0]):d}/" + \
-            f"{int(mylat[1]):d}/{int(mylon[1]):d}"
-        #[60, -10, 50, 2], # North, West, South, East
-        c = cdsapi.Client()                
-        c.retrieve('reanalysis-era5-single-levels',
-        {
-            'format': 'netcdf',
-            'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', 
-                        '2m_dewpoint_temperature', '2m_temperature', 'evaporation', 
-                        'potential_evaporation', 'surface_solar_radiation_downwards',
-                        'total_precipitation', 'volumetric_soil_water_layer_1',
-                        'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3',
-                        'volumetric_soil_water_layer_4',
-                        ],
-            'product_type':'reanalysis',
-            'year':f"{year:d}",
-            'month':f"{month:02d}",
-            'day':[
-                '01','02','03',
-                '04','05','06',
-                '07','08','09',
-                '10','11','12',
-                '13','14','15',
-                '16','17','18',
-                '19','20','21',
-                '22','23','24',
-                '25','26','27',
-                '28','29','30',
-                '31'
-            ],
-            'time':[
-                '00:00','01:00','02:00',
-                '03:00','04:00','05:00',
-                '06:00','07:00','08:00',
-                '09:00','10:00','11:00',
-                '12:00','13:00','14:00',
-                '15:00','16:00','17:00',
-                '18:00','19:00','20:00',
-                '21:00','22:00','23:00'
-            ],
-            #'area' : "%d/%d/%d/%d"%
-            'area' : area,
-            'format':'netcdf'
+        area = (
+            f"{int(mylat[0]):d}/{int(mylon[0]):d}/"
+            + f"{int(mylat[1]):d}/{int(mylon[1]):d}"
+        )
+        # [60, -10, 50, 2], # North, West, South, East
+        c = cdsapi.Client()
+        c.retrieve(
+            "reanalysis-era5-single-levels",
+            {
+                "format": "netcdf",
+                "variable": [
+                    "10m_u_component_of_wind",
+                    "10m_v_component_of_wind",
+                    "2m_dewpoint_temperature",
+                    "2m_temperature",
+                    "evaporation",
+                    "potential_evaporation",
+                    "surface_solar_radiation_downwards",
+                    "total_precipitation",
+                    "volumetric_soil_water_layer_1",
+                    "volumetric_soil_water_layer_2",
+                    "volumetric_soil_water_layer_3",
+                    "volumetric_soil_water_layer_4",
+                ],
+                "product_type": "reanalysis",
+                "year": f"{year:d}",
+                "month": f"{month:02d}",
+                "day": [f"{day:02d}" for day in range(1, 32)],
+                "time": [f"{hour:02d}:00" for hour in range(0, 24)],
+                "area": area,
+                "format": "netcdf",
             },
-        output_nc_file.as_posix())
+            output_nc_file.as_posix(),
+        )
         return output_nc_file.as_posix()
     else:
+        LOG.info(f"Skipping {year}-{month}")
         return None
 
-        
+
 if __name__ == "__main__":
-    #(-3.262065, 4.738830) - (1.200134, 11.165904)
+    # (-3.262065, 4.738830) - (1.200134, 11.165904)
     # Ghana extent
     mylat = [1, 12]
     mylon = [-3, 5]
-    output_folder = "/gws/nopw/j04/odanceo/public/ERA5_meteo/"
-    wrapper = partial (grab_era5, region="Ghana", output_folder=output_folder, 
-                       mylat=mylat, mylon=mylon)
-    
+    output_folder = "/gws/nopw/j04/odanceo/public/ERA5_meteo/netcdf/"
+    wrapper = partial(
+        grab_era5,
+        region="Ghana",
+        output_folder=output_folder,
+        mylat=mylat,
+        mylon=mylon,
+    )
+
     # create a thread pool of 4 threads
-    years = np.arange(2000,2021).astype(np.int)
+    years = np.arange(2000, dt.datetime.now().year + 1).astype(np.int)
     months = np.arange(1, 13).astype(np.int)
     with ThreadPoolExecutor(max_workers=8) as executor:
         for year in years:
