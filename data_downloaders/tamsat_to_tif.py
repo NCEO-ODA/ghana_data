72747899ff1d18b4592d8e816c96621a32b5c89b
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-08-02 22:00:51 +0100

    Also does smcl layers. Painful to witness code

--- data_downloaders/tamsat_to_tif.py
+++ data_downloaders/tamsat_to_tif.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 import subprocess
+from pathlib import Path
 
 import gdal
 import numpy as np
@@ -13,6 +15,7 @@ proj = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563
 
 
 for year in range(2000, 2021):
+    print(f"Doing year {year}")
     cmd = [
         "cdo",
         "cat",
@@ -27,7 +30,9 @@ for year in range(2000, 2021):
     if p.returncode != 0:
         raise subprocess.CalledProcessError(p.returncode, p.args)
 
-    ds = xr.open_dataset(f"{year}.nc", chunks={})
+    ds = xr.open_dataset(
+        f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/{year}.nc", chunks={}
+    )
 
     tsteps = [
         x.strftime("%Y-%m-%d")
@@ -38,6 +43,7 @@ for year in range(2000, 2021):
         for x in pd.to_datetime(ds.coords["time"].values)
     ]
 
+    print(f"Creating tiffs...")
     for variable in [
         "precip",
         "esoil_gb",
@@ -45,6 +51,11 @@ for year in range(2000, 2021):
         "smc_avail_top",
         "runoff",
     ]:
+        if Path(
+            f"/gws/nopw/j04/odanceo/public/sm_tiff/tamsat_{variable}_{year}.tif"
+        ).exists():
+            print(f"Skipping tamsat_{variable}_{year}.tif")
+            continue
         drv = gdal.GetDriverByName("GTiff")
         dst_ds = drv.Create(
             f"/gws/nopw/j04/odanceo/public/sm_tiff/tamsat_{variable}_{year}.tif",
@@ -66,6 +77,35 @@ for year in range(2000, 2021):
             this_band = dst_ds.GetRasterBand(band + 1)
             this_band.SetMetadata({"DoY": "%03d" % doy, "Date": tstep})
             x = ds[variable].isel({"time": band}).values
+            x[np.isnan(x)] = -9999
+            this_band.WriteArray(x)
+            this_band.SetNoDataValue(-9999)
+        dst_ds = None
+
+    vari = "smcl"
+    for layer in range(4):
+        variable = f"{vari}_{layer+1}"
+        drv = gdal.GetDriverByName("GTiff")
+        dst_ds = drv.Create(
+            f"/gws/nopw/j04/odanceo/public/sm_tiff/tamsat_{variable}_{year}.tif",
+            278,
+            292,
+            len(doys),
+            gdal.GDT_Float32,
+            options=[
+                "COMPRESS=DEFLATE",
+                "TILED=YES",
+                "BIGTIFF=YES",
+                "PREDICTOR=1",
+            ],
+        )
+        dst_ds.SetGeoTransform(geoT)
+        dst_ds.SetProjection(proj)
+
+        for band, (tstep, doy) in enumerate(zip(tsteps, doys)):
+            this_band = dst_ds.GetRasterBand(band + 1)
+            this_band.SetMetadata({"DoY": "%03d" % doy, "Date": tstep})
+            x = ds[vari][:, layer, :, :].isel({"time": band}).values
             x[np.isnan(x)] = -9999
             this_band.WriteArray(x)
             this_band.SetNoDataValue(-9999)
