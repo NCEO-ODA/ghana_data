301b9f9e2c85d79358b3c55e28e1557fd560bdf9
Jose Gomez-Dans <jgomezdans@gmail.com>
2020-08-06 21:35:28 +0100

    Add some meat

--- data_analysis/calc_climatologies.py
+++ data_analysis/calc_climatologies.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
 
+import numpy as np
+import gdal
 
-from calc_climatologies import ERA5_VARIABLES, MODIS_VARIABLES, TAMSAT_VARIABLES
+from calc_climatologies import ERA5_VARIABLES, MODIS_VARIABLES
+from calc_climatologies import TAMSAT_VARIABLES
+
+from calc_climatologies import get_modis_ds
+from calc_climatologies import get_era5_ds
+from calc_climatologies import get_tamsat_ds
+
+
+if __name__ == "__main__":
+    
+    clim_periods = {'long':[2002, 2019],
+                    'recent':[2015, 2019],
+                    'past': [2002, 2010]
+                    }
+    for variable in ERA5_VARIABLES:
+        ds = get_era5_ds(variable)
+        for k, v in clim_periods.items():
+            m, s = calculate_climatology(ds, first_year=v[0], period="time.month", last_year=v[1])
+            break
+        break
+        
+    
