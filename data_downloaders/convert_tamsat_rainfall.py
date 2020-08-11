970cbceb5185ecbaa737fa954bd621e3c3ee6be1
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2020-08-11 14:53:28 +0100

    Convert TAMSAT product rainfall
    This script converts the original TAMSAT rainfall data to GeoTIFF.
    
    Looks reasonable, and closes #15

---
+++ data_downloaders/convert_tamsat_rainfall.py
@@ -0,0 +1,95 @@
+mport subprocess
+from pathlib import Path
+
+import gdal
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+gdal.UseExceptions()
+
+#geoT = [-17.875, 0.25, 0, 37.375, 0.0, -0.25]
+#geoT = [-19.0125, 0.0375, 0, 38.025, 0.0, -0.0375]
+geoT = [-18.875, 0.25, 0, 38.875, 0.0, -0.25]
+
+proj = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+
+
+for year in range(2000, 2020):
+    print(f"Doing year {year}")
+    nc_cat = Path(f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/{year}.nc")
+    if nc_cat.exists():
+        print("Removing stale annual NetCDF file...")
+        nc_cat.unlink()  # In case we end up adding more and more copies of same date
+    cmd = [
+        "cdo",
+        "cat",
+        f"/gws/nopw/j04/tamsat/public/rfe/data_degraded/v3.1/daily/0.25/{year}/??/rfe{year}_??_??_0.25.v3.1.nc",
+        #f"/gws/nopw/j04/tamsat/public/rfe/data/v3.1/daily/{year}/??/rfe{year}_??_??.v3.1.nc",
+        # f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/tamsat_nceo_da_wb_v1p0p0_{year}????.nc",
+        f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/{year}_rfe.nc",
+    ]
+    with subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True
+    ) as p:
+        for line in p.stdout:
+            print(line, end="")  # process line here
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(p.returncode, p.args)
+
+    ds = xr.open_dataset(
+        f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/{year}_rfe.nc",
+        decode_times=True,
+        decode_cf=True,
+        mask_and_scale=True,
+        decode_coords=True,
+        chunks={"time": 1},
+    )
+
+    tsteps = [
+        x.strftime("%Y-%m-%d")
+        for x in pd.to_datetime(ds.coords["time"].values)
+    ]
+    doys = [
+        int(x.strftime("%j"))
+        for x in pd.to_datetime(ds.coords["time"].values)
+    ]
+
+    print(f"Creating tiffs...")
+    for variable in [
+        "rfe_filled"
+    ]:
+        if Path(
+            f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/GTiff/tamsat_{variable}_{year}.tif"
+        ).exists():
+            print(f"Skipping tamsat_{variable}_{year}.tif")
+            # continue
+            pass
+        drv = gdal.GetDriverByName("GTiff")
+        dst_ds = drv.Create(
+            f"/gws/nopw/j04/odanceo/public/soil_moisture/nc/GTiff/tamsat_{variable}_{year}.tif",
+            288,
+            300,
+            len(doys),
+            gdal.GDT_Float32,
+            options=[
+                "COMPRESS=DEFLATE",
+                "TILED=YES",
+                "BIGTIFF=YES",
+                "PREDICTOR=1",
+            ],
+        )
+        dst_ds.SetGeoTransform(geoT)
+        dst_ds.SetProjection(proj)
+
+        for band, (tstep, doy) in enumerate(zip(tsteps, doys)):
+            this_band = dst_ds.GetRasterBand(band + 1)
+            this_band.SetMetadata({"DoY": "%03d" % doy, "Date": tstep})
+            x = ds[variable].isel({"time": band}).values
+            x[x == x[0, 0]] = np.nan
+            x[np.isnan(x)] = -9999
+            this_band.WriteArray(np.flipud(x))
+            this_band.SetNoDataValue(-9999)
+        dst_ds = None
+
+
